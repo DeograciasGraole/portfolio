f289c9c644391dd7e3ac8c38737413bfa6811779
Deogracias <graolewillwamp@gmail.com>
2025-11-20 16:43:17 +0200

    hh

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -114,7 +114,7 @@ const Hero = () => {
             >
               <div className="spotlight-glow"></div>
               <img
-                src={aboutPic}
+                src="../assets/aboutmepic.png"
                 alt="Devanta Ebison portrait"
                 loading="lazy"
               />
