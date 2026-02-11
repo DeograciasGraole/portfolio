e0fcd14180637f39f9063ae63b5a2b21ab9836e1
Deogracias <graolewillwamp@gmail.com>
2026-02-12 01:17:22 +0200

    hh

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,7 +17,7 @@ const Hero = () => {
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.8, delay: 0.2 }}
           >
-            SOFTWARE CODER
+            SOFTWARES CODER
           </motion.h1>
 
           <div className="hero-intro">
